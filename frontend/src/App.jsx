0f121373fa85c699aff25bcaf8aa6dacafc52dc4
PARAG <paragdakhane123@gmail.com>
2025-03-30 15:54:19 +0530

    some changes in setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,44 +1,13 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Login from "./pages/Auth/Login";
-import SignUp from "./pages/Auth/SignUp";
-import PrivateRoutes from "./routes/PrivateRoutes";
-import MyTasks from "./pages/Users/MyTasks";
-import UserDashboard from "./pages/Users/UserDashboard";
-import Dashboard from "./pages/Admin/Dashboard";
-import ManageUsers from "./pages/Admin/ManageUsers";
-import CreateTask from "./pages/Admin/CreateTask";
-import ManageTask from "./pages/Admin/ManageTask";
-import TaskDetails from "./pages/Users/TaskDetails";
+import React from 'react'
+import Login from './pages/Login.jsx'
 
-
-
-function App() {
+const App = () => {
   return (
     <>
-      <Router>
-        <Routes>
-          <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<SignUp />} />
-
-          <Route element={<PrivateRoutes allowedUsers={["admin"]} />}>
-            <Route path="/admin/dashboard" element={<Dashboard />} />
-            <Route path="/admin/users" element={<ManageUsers />} />
-            <Route path="/admin/create-task" element={<CreateTask />} />
-            <Route path="/admin/tasks" element={<ManageTask />} />
-          </Route>
-
-          <Route element={<PrivateRoutes allowedUsers={["user"]} />}>
-            <Route path="/user/dashboard" element={<UserDashboard />} />
-            <Route path="/user/tasks" element={<MyTasks />} />
-            <Route
-              path="/user/task-details/:id"
-              element={<TaskDetails />}
-            />
-          </Route>
-        </Routes>
-      </Router>
+    <Login></Login>
+    <h1>Parag is lit</h1>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
